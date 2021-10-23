3543f5d7da3ea8097389475d80464604f656fa06
hmallen <allenhm@gmail.com>
2021-10-23 10:50:23 +0000

    Calibration functions work in progress.

--- BeanbagNeutralizer.ino
+++ BeanbagNeutralizer.ino
@@ -6,6 +6,15 @@
 
 #include <Arduino.h>
 #include <ServoEasing.h>
+#include <Preferences.h>
+
+#define SERVO_MIN_DEGREE 0
+#define SERVO_MAX_DEGREE 270
+
+#define TOUCH_PIN T0
+#define TOUCH_THRESHOLD 40
+
+Preferences preferences;
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASS;
@@ -13,11 +22,17 @@ const char* password = WIFI_PASS;
 AsyncWebServer server(80);
 
 const int ledPin = 2;
+const int potPin = 32;
 const int strobePin = 33;
-const int sirenPin = 32;
+const int sirenPin = 23;
 const int laserPin = 25; // A0
 const int xServoPin = 5;
 const int yServoPin = 18;
+
+int potMin;
+int potMax;
+int xMin, xMax;
+int yMin, yMax;
 
 struct ServoControlStruct {
   uint16_t minDegree;
@@ -36,6 +51,57 @@ bool laserActive = false;
 uint32_t strobeStart;
 uint32_t sirenStart;
 uint32_t laserStart;
+
+uint16_t strobeDuration;
+uint16_t sirenDuration;
+uint16_t laserDuration;
+
+void mqttCallback(char* topic, byte* payload, uint8_t length) {
+  Serial.print(F("MQTT message received ["));
+  Serial.print(topic);
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void calibratePot() {
+  Serial.println(F("Calibrating potentiometer."));
+
+  minSet = false;
+  maxSet = false;
+
+  Serial.println(F("Move potentiometer to MIN position then tap touch sensor to confirm."));
+  while (minSet == false) {
+    if (touchRead(TOUCH_PIN) < TOUCH_THRESHOLD) {
+      while (touchRead(TOUCH_PIN) < TOUCH_THRESHOLD) {
+        delay(10);
+      }
+      potMin = analogRead(potPin);
+      minSet = true;
+    }
+  }
+
+  Serial.println(F("Move potentiometer to MAX position then tap touch sensor to confirm."));
+  while (maxSet == false) {
+    if (touchRead(TOUCH_PIN) < TOUCH_THRESHOLD) {
+      while (touchRead(TOUCH_PIN) < TOUCH_THRESHOLD) {
+        delay(10);
+      }
+      potMax = analogRead(potPin);
+      maxSet = true;
+    }
+  }
+}
+
+void calibrateServos() {
+  Serial.println(F("Calibrating servos."));
+
+  xSet = false;
+  ySet = false;
+
+
+}
 
 void setup(void) {
   // Built-in LED
@@ -61,6 +127,32 @@ void setup(void) {
   pinMode(yServoPin, OUTPUT);
 
   Serial.begin(115200);
+  Serial.println();
+
+  preferences.begin("bbneut", false);
+
+  potMin = preferences.getInt("potMin", -1);
+  potMax = preferences.getInt("potMax", -1);
+
+  if (potMin == -1 || potMax == -1) {
+    digitalWrite(ledPin, HIGH);
+    calibratePot();
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(1000);
+
+  if (touchRead(TOUCH_PIN) < TOUCH_THRESHOLD) {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    while (touchRead(TOUCH_PIN) < TOUCH_THRESHOLD) {
+      delay(10);
+    }
+    digitalWrite(ledPin, HIGH);
+    calibratePot();
+    digitalWrite(ledPin, LOW);
+  }
 
   Serial.print(F("Attach servo at pin "));
   Serial.println(xServoPin);
@@ -73,10 +165,46 @@ void setup(void) {
   if (ServoY.attach(yServoPin, 67) == INVALID_SERVO) {
     Serial.println(F("Error attaching servo"));
     while (true) {
-      digitalWrite(LED_BUILTIN, HIGH);
+      digitalWrite(ledPin, HIGH);
       delay(100);
-      digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(ledPin, LOW);
       delay(100);
+    }
+  }
+
+  delay(1000);
+
+  int touchDetected = 0;
+  for (int i = 0; i < 5; i++) {
+    if (touchRead(TOUCH_PIN) < TOUCH_THRESHOLD) {
+      digitalWrite(ledPin, HIGH);
+      delay(100);
+      digitalWrite(ledPin, LOW);
+      touchDetected++;
+    }
+    delay(500);
+    if (touchDetected >= 3) {
+      digitalWrite(ledPin, HIGH);
+      calibrateServos();
+      digitalWrite(ledPin, LOW);
+      break;
+    }
+  }
+  else {
+    xMin = preferences.getInt("xMin", -1);
+    xMax = preferences.getInt("xMax", -1);
+    yMin = preferences.getInt("yMin", -1);
+    yMax = preferences.getInt("yMax", -1);
+
+    if (
+      xMin == -1 ||
+      xMax == -1 ||
+      yMin == -1 ||
+      yMax == -1
+    ) {
+      digitalWrite(ledPin, HIGH);
+      calibrateServos();
+      digitalWrite(ledPin, LOW);
     }
   }
 
